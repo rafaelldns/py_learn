c98f852b861d85fb5ce35873b9d148c6b95f1afc
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-04-27 11:44:04 -0300

    Add input/output

---
+++ inputs-outputs.py
@@ -0,0 +1,11 @@
+name = input("Name: ")
+age = input("Age: ")
+grad = input("Graduation: ")
+city = input("City: ")
+work = input("Work: ")
+status = input("Status: ")
+
+def profile(name, age, grad, city, work, status):
+    print(f"Your name is {name}, you are {age} years old, lives in {city}, is formed in {grad}, work with {work} and is {status}")
+
+profile(name, age, grad, city, work, status)
