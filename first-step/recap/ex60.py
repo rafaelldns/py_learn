485a94938b2ec7cecfa85cbce5445c49d4efd987
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-09-16 22:24:30 -0300

    recap

--- first-step/recap/ex60.py
+++ first-step/recap/ex60.py
@@ -1,10 +1,20 @@
-asasasasa
-asas
-asas
-asas
-asas
-asas
-asasas
-asasasasasas
-asasasasasas
-asasasasaasasasasaasassa
+print('== CHALLENGE 60 ==')
+
+n = int(input('Insert a number for factorial: '))
+print('{}!'.format(n))
+
+var = []
+
+for i in range(n, 0, -1):
+    var.append(str(i))
+
+rvar = ' x '.join(var)
+print(rvar)
+
+fact = n
+
+while n != 1:
+    fact *= (n-1)
+    n -= 1
+
+print('The factorial is: {}'.format(fact))
