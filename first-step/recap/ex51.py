2bd4950d95a948bfad8505235f2f5795fee2762e
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-09-16 22:50:46 -0300

    recap

--- first-step/recap/ex51.py
+++ first-step/recap/ex51.py
@@ -1,12 +1,13 @@
 print('== CHALLENGE 50 ==')
 f = int(input('Enter the first term: '))
 r = int(input('Enter the reason: '))
+t = f + (10-1) * r
 
 p = []
 a = f
 
 print('\nPrinting the first 10 terms of this progression:\n')
-for i in range(0,10):
+for i in range(f,t,r):
     p.append(str(a))
     a += r
 
---
+++ first-step/recap/ex61.py
@@ -0,0 +1,28 @@
+print('== CHALLENGE 61 ==')
+
+first = int(input('Enter the first term: '))
+reason = int(input('Enter the reason: '))
+max = 0
+pa = first
+pro = []
+
+while max <10 : 
+        pro.append(str(pa))
+        pa += reason
+        max +=1
+
+rpro = ' -> '.join(pro)
+print('This 10 terms PA:\n{}'.format(rpro))
+
+more = 1
+
+while more != 0:
+    more = int(input('How many more terms do you want(0 to exit)? '))
+    tempmore = more
+    pro2 = []
+    while tempmore != 0:
+        pro2.append(str(pa))
+        pa += reason 
+        tempmore -= 1
+    rpro2 = ' -> '.join(pro2)
+    print(rpro2)
