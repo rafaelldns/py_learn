c935e7ee4f25a118ac4cdb847511e2ae767d9eb8
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-09-19 11:05:57 -0300

    recap

--- first-step/recap/ex71.py
+++ first-step/recap/ex71.py
@@ -2,77 +2,17 @@ print('== CHALLENGE 71 ==')
 
 print('\n======= ATM =======\n')
 
+notes_disp = [50,20,10,1]
+banknotes = {}
+
 value = int(input('Insert a value to withdraw: '))
 
-rest_value = 0
+for note in notes_disp:
+    if value >= note:
+        quantity = value // note
+        banknotes[note] = quantity
+        value = value % note
 
-while True:
-    if value > 50:
-        fifht_note = value // 50
-        rest_value = value % 50
-        if rest_value > 20:
-            twenty_note = rest_value // 20
-            rest_value = rest_value % 20 
-            if rest_value > 10:
-                ten_note = rest_value // 10
-                rest_value = rest_value % 10
-                if rest_value > 1:
-                    one_note = rest_value//1
-                    break
-                else:
-                    break
-            elif rest_value > 1:
-                one_note = rest_value//1
-                break
-            else:
-                break
-        elif rest_value > 10:
-            ten_note = rest_value // 10
-            rest_value = rest_value % 10
-            if rest_value > 1:
-                one_note = rest_value//1
-                break
-            else:
-                break
-        elif rest_value > 1:     
-            one_note = rest_value//1
-            break
-        else:
-            break
-    elif value > 20:
-        twenty_note = value // 20
-        rest_value = value % 20
-        if rest_value > 10:
-            ten_note = rest_value //10
-            rest_value = rest_value % 10
-            if rest_value > 1:
-                one_note = rest_value//1
-                break
-            else:
-                break
-        elif rest_value > 1:
-            one_note = rest_value//1
-            break
-        else:
-            break
-    elif value > 10:
-        ten_note = value //10
-        rest_value = value % 10
-        if rest_value > 1:
-            one_note = rest_value//1
-            break
-        else:
-            break
-    elif value > 1:
-        one_note = value//1
-        break
-    else:
-        break
-
-print(f'''
-=== Banknotes to be printed ===
-notes -> $50 : {fifht_note}
-notes -> $20 : {twenty_note}
-notes -> $10 : {ten_note}
-notes -> $1 : {one_note}
-''')
+for note, quantity in banknotes.items():
+    print(f'Quantity for notes ${note}: {quantity}')
+    
