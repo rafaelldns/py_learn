5cca9430a4eba6ae3237273646c7ecd7e8e53d41
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-09-23 11:21:57 -0300

    organize

---
+++ first-step/recap/primitive/ex4.py
@@ -0,0 +1,9 @@
+print('== CHALLENGE 4 ==')
+n = input('Insert a Number = ')
+
+print("Numeric? ", n.isnumeric())
+print('Alpha?', n.isalpha())
+print('Alphanum?', n.isalnum())
+print('Lower?', n.islower())
+print('Upper?', n.isupper())
+print('Space?', n.isspace())
